6a44a622fd1bc242f454f1bd61a72b2fcdb0b314
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-03-25 22:18:46 -0500

    better iteration of diffsquares solution

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -3,24 +3,15 @@ package diffsquares
 
 // SumOfSquares finds sum of squares
 func SumOfSquares(n int) int {
-	sumsq := 0
-	for i := 1; i <= n; i++ {
-		sumsq += i * i
-	}
-
-	return sumsq
+	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
 // SquareOfSums square of the sums
 func SquareOfSums(n int) int {
-	sqsums := 0
-	for i := 1; i <= n; i++ {
-		sqsums += i
-	}
-	return sqsums * sqsums
+	return ((n * (n + 1)) / 2 * (n * (n + 1)) / 2)
 }
 
-// Difference
+// Difference finds the difference of sqsums ans sumofsqs
 func Difference(n int) int {
 	return SquareOfSums(n) - SumOfSquares(n)
 }
