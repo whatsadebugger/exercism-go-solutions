f5464ab4392e7a0cfaea2590160018be42526bba
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-03-28 20:12:46 -0500

    exercism luhn final solution

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -8,7 +8,7 @@ import (
 
 // Valid validates if a number is valid per Luhn formula
 func Valid(s string) bool {
-	s = strings.TrimSpace(s)
+	s = strings.Replace(s, " ", "", -1)
 
 	if _, err := strconv.ParseInt(s, 10, 0); err != nil || len(s) < 2 {
 		return false
@@ -18,8 +18,24 @@ func Valid(s string) bool {
 }
 
 func luhn(s string) bool {
+	nums := strings.Split(s, "")
+	sum := 0
+	double := false
+	for i := len(s) - 1; i >= 0; i-- {
+		v, _ := strconv.ParseInt(nums[i], 10, 8)
+		n := int(v)
 
-	strings.Split(s, "")
-	return false
+		if double {
+			if n*2 >= 10 {
+				n = n*2 - 9
+			} else {
+				n = n * 2
+			}
+		}
 
+		double = !double
+		sum += n
+	}
+
+	return sum%10 == 0
 }
