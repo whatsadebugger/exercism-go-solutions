ee532f622967f14a80340e55f98845f7fe57fb22
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-07-03 06:54:49 -0500

    parallel letters

--- go/parallel-letter-frequency/frequency.go
+++ go/parallel-letter-frequency/frequency.go
@@ -1,5 +1,9 @@
 package letter
 
+import "sync"
+
+var mapMutex = &sync.Mutex{}
+
 type FreqMap map[rune]int
 
 func Frequency(s string) FreqMap {
@@ -7,5 +11,28 @@ func Frequency(s string) FreqMap {
 	for _, r := range s {
 		m[r]++
 	}
+	return m
+}
+
+func ConcurrentFrequency(strings []string) FreqMap {
+	m := FreqMap{}
+	wg := sync.WaitGroup{}
+
+	wg.Add(len(strings))
+
+	for _, v := range strings {
+		go func(text string) {
+			defer wg.Done()
+			fm := Frequency(text)
+			mapMutex.Lock()
+			for k, value := range fm {
+				m[k] += value
+			}
+			mapMutex.Unlock()
+		}(v)
+	}
+
+	wg.Wait()
+
 	return m
 }
